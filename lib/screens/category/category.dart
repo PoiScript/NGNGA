5c4e7925b7daa2546a9ac0bc62e2aac4ed6a85e0
PoiScript <poiscript@gmail.com>
2019-11-10 15:59:54 +0800

    feat(category,topic): implement inifinite scroll

--- lib/screens/category/category.dart
+++ lib/screens/category/category.dart
@@ -6,32 +6,145 @@ import '../../models/topic.dart';
 import '../../models/category.dart';
 import '../../store/state.dart';
 import '../../store/fetch_topics.dart';
+import '../../widgets/flexible_title.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  final Category category;
   final List<Topic> topics;
+  final int topicsCount;
+  final bool isLoading;
 
-  CategoryPage({Key key, this.topics}) : super(key: key);
+  final Future<void> Function() onRefresh;
+  final VoidCallback fetchNext;
+
+  bool get containsMaxPage => (topics.length ~/ 20) == (topicsCount ~/ 20);
+
+  CategoryPage({
+    Key key,
+    @required this.topics,
+    @required this.category,
+    @required this.topicsCount,
+    @required this.isLoading,
+    @required this.onRefresh,
+    @required this.fetchNext,
+  })  : assert(topics != null),
+        assert(category != null),
+        assert(topicsCount != null),
+        assert(isLoading != null),
+        assert(onRefresh != null),
+        assert(fetchNext != null),
+        super(key: key);
+
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+const kExpandedHeight = 200.0;
+
+class _CategoryPageState extends State<CategoryPage> {
+  ScrollController _scrollController;
+  double _scale = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        if (!_scrollController.hasClients) {
+          _scale = 0.0;
+        } else if (_scrollController.offset <=
+            kExpandedHeight - kToolbarHeight) {
+          _scale =
+              _scrollController.offset / (kExpandedHeight - kToolbarHeight);
+        } else {
+          _scale = 1.0;
+        }
+
+        if (!widget.isLoading &&
+            _scrollController.offset ==
+                _scrollController.position.maxScrollExtent) {
+          widget.fetchNext();
+        }
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (widget.isLoading && widget.topics.isEmpty) {
+      return Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
-      body: topics.isNotEmpty
-          ? RefreshIndicator(
-              onRefresh: () {
-                return;
-              },
-              child: _buildList(context, topics),
-            )
-          : Center(child: CircularProgressIndicator()),
+      body: RefreshIndicator(
+        onRefresh: widget.onRefresh,
+        child: CustomScrollView(
+          controller: _scrollController,
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: kExpandedHeight,
+              floating: false,
+              pinned: true,
+              leading: const BackButton(color: Colors.black),
+              backgroundColor: Theme.of(context).cardColor,
+              actions: <Widget>[
+                IconButton(
+                  color: Colors.black,
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {},
+                ),
+              ],
+              flexibleSpace: FlexibleTitle(
+                scale: _scale,
+                title: widget.category.title,
+                // subtitle: Text(
+                //   "${widget.topicsCount} topics",
+                //   style: Theme.of(context).textTheme.caption,
+                // ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  if (index == widget.topics.length) {
+                    if (widget.containsMaxPage) {
+                      return null;
+                    } else {
+                      return _buildIndicator();
+                    }
+                  } else {
+                    var topic = widget.topics[index];
+                    return GestureDetector(
+                      onTap: () => _onTap(context, topic.id),
+                      child: TopicRow(topic),
+                    );
+                  }
+                },
+                childCount: widget.topics.length + 1,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  Widget _buildList(BuildContext context, List<Topic> topics) {
-    return ListView.builder(
-      itemCount: topics.length,
-      itemBuilder: (context, index) => GestureDetector(
-        onTap: () => _onTap(context, topics[index].id),
-        child: TopicRow(topics[index]),
+  Widget _buildIndicator() {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: SizedBox(
+          width: 24.0,
+          height: 24.0,
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation(Colors.black),
+          ),
+        ),
       ),
     );
   }
@@ -50,13 +163,17 @@ class CategoryPageConnector extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreConnector<AppState, ViewModel>(
       model: ViewModel(categoryId),
-      // distinct: false,
-      onInit: (st) => st.dispatch(FetchTopicsAction(
-        category: Category(id: categoryId),
-        page: 0,
+      onInit: (store) => store.dispatch(FetchTopicsAction(
+        categoryId: categoryId,
       )),
-      builder: (BuildContext context, ViewModel vm) =>
-          CategoryPage(topics: vm.topics ?? []),
+      builder: (BuildContext context, ViewModel vm) => CategoryPage(
+        isLoading: vm.isLoading,
+        topics: vm.topics,
+        category: vm.category,
+        topicsCount: vm.topicsCount,
+        onRefresh: vm.onRefresh,
+        fetchNext: vm.fetchNext,
+      ),
     );
   }
 }
@@ -66,29 +183,35 @@ class ViewModel extends BaseModel<AppState> {
 
   ViewModel(this.categoryId);
 
+  Category category;
   List<Topic> topics;
+  int topicsCount;
   bool isLoading;
+
   Future<void> Function() onRefresh;
+  VoidCallback fetchNext;
 
   ViewModel.build({
     @required this.topics,
     @required this.isLoading,
     @required this.categoryId,
+    @required this.category,
+    @required this.topicsCount,
     @required this.onRefresh,
-  }) : super(equals: [isLoading, topics]);
+    @required this.fetchNext,
+  }) : super(equals: [isLoading, topics, category, topicsCount]);
 
   @override
   ViewModel fromStore() {
     return ViewModel.build(
-      topics: state.categories[categoryId]?.topicIds
-          ?.map((id) => state.topics[id].topic)
-          ?.toList(),
+      category: state.categories[categoryId].category,
+      topics: state.categories[categoryId].topics,
       isLoading: state.isLoading,
-      onRefresh: () => dispatchFuture(FetchTopicsAction(
-        category: Category(id: categoryId),
-        page: 0,
-      )),
+      onRefresh: () =>
+          dispatchFuture(FetchTopicsAction(categoryId: categoryId)),
+      fetchNext: () => dispatch(FetchNextTopicsAction(categoryId: categoryId)),
       categoryId: categoryId,
+      topicsCount: state.categories[categoryId].topicsCount,
     );
   }
 }
--- lib/screens/topic/topic.dart
+++ lib/screens/topic/topic.dart
@@ -1,52 +1,152 @@
+import 'dart:collection';
+
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/flutter_widgets.dart';
 
+import './post_row.dart';
 import '../../models/topic.dart';
 import '../../models/post.dart';
-import './post_row.dart';
-
 import '../../store/state.dart';
 import '../../store/fetch_posts.dart';
 
-class TopicPage extends StatelessWidget {
-  final Map<int, List<Post>> pages;
+class TopicPage extends StatefulWidget {
+  final ListQueue<Post> posts;
   final Topic topic;
+  final bool isLoading;
 
-  TopicPage({this.topic, this.pages});
+  final VoidCallback fetchPrevious;
+  final VoidCallback fetchNext;
+
+  bool get containsMinPage => (posts.first.index ~/ 20) == 0;
+  bool get containsMaxPage =>
+      (posts.last.index ~/ 20) == (topic.postsCount ~/ 20);
+
+  TopicPage({
+    @required this.topic,
+    @required this.posts,
+    @required this.isLoading,
+    @required this.fetchNext,
+    @required this.fetchPrevious,
+  })  : assert(isLoading != null),
+        assert(fetchNext != null),
+        assert(fetchPrevious != null);
+
+  @override
+  _TopicPageState createState() => _TopicPageState();
+}
+
+class _TopicPageState extends State<TopicPage> {
+  ItemPositionsListener _itemPositionsListener;
+
+  @override
+  void initState() {
+    _itemPositionsListener = ItemPositionsListener.create()
+      ..itemPositions.addListener(() {
+        var value = _itemPositionsListener.itemPositions.value;
+        if (!widget.isLoading) {
+          // print("${value.last.index} == ${widget.posts.length}");
+          if (!widget.containsMinPage && value.first.index == 0) {
+            widget.fetchPrevious();
+          } else if (!widget.containsMaxPage &&
+              value.last.index == widget.posts.length) {
+            widget.fetchNext();
+          }
+        }
+      });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (widget.isLoading && widget.topic == null) {
+      return Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(topic.title)),
-      body: pages.isNotEmpty
-          ? ListView(
-              children: pages.values
-                  .expand((posts) => posts)
-                  .map((post) => PostRow(post))
-                  .toList(),
-            )
-          : Center(child: CircularProgressIndicator()),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          widget.topic.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        leading: const BackButton(color: Colors.black),
+        actions: <Widget>[
+          IconButton(
+            color: Colors.black,
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        titleSpacing: 0.0,
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {},
+        child: ScrollablePositionedList.builder(
+          initialScrollIndex: 1,
+          itemCount: widget.posts.length + 2,
+          itemPositionsListener: _itemPositionsListener,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              if (widget.containsMinPage) {
+                return null;
+              } else {
+                return _buildIndicator();
+              }
+            } else if (index == widget.posts.length + 1) {
+              if (widget.containsMaxPage) {
+                return null;
+              } else {
+                return _buildIndicator();
+              }
+            } else {
+              return PostRow(widget.posts.elementAt(index - 1));
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIndicator() {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: SizedBox(
+          width: 24.0,
+          height: 24.0,
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation(Colors.black),
+          ),
+        ),
+      ),
     );
   }
 }
 
 class TopicPageConnector extends StatelessWidget {
   final int topicId;
+  final int pageIndex;
 
-  TopicPageConnector(this.topicId);
+  TopicPageConnector(this.topicId, {this.pageIndex = 1})
+      : assert(pageIndex != null && pageIndex > 0);
 
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, ViewModel>(
       model: ViewModel(topicId),
-      // distinct: false,
-      onInit: (st) => st.dispatch(FetchPostsAction(
-        topic: Topic(id: topicId),
-        page: 0,
+      onInit: (store) => store.dispatch(FetchPostsAction(
+        topicId: topicId,
+        pageIndex: pageIndex,
       )),
       builder: (BuildContext context, ViewModel vm) => TopicPage(
-        pages: vm.pages ?? Map(),
+        posts: vm.posts,
         topic: vm.topic,
+        isLoading: vm.isLoading,
+        fetchNext: vm.fetchNext,
+        fetchPrevious: vm.fetchPrevious,
       ),
     );
   }
@@ -56,25 +156,34 @@ class ViewModel extends BaseModel<AppState> {
   final topicId;
 
   Topic topic;
-  Map<int, List<Post>> pages;
+  ListQueue<Post> posts;
+
   bool isLoading;
+
+  VoidCallback fetchPrevious;
+  VoidCallback fetchNext;
 
   ViewModel(this.topicId);
 
   ViewModel.build({
-    @required this.pages,
+    @required this.posts,
     @required this.isLoading,
     @required this.topicId,
     @required this.topic,
-  }) : super(equals: [isLoading, pages, topic]);
+    @required this.fetchPrevious,
+    @required this.fetchNext,
+  }) : super(equals: [isLoading, posts, topic]);
 
   @override
   ViewModel fromStore() {
+    var topic = state.topics[topicId];
     return ViewModel.build(
-      pages: state.topics[topicId]?.pages,
-      topic: state.topics[topicId]?.topic,
+      posts: topic?.posts,
+      topic: topic?.topic,
       isLoading: state.isLoading,
       topicId: topicId,
+      fetchPrevious: () => dispatch(FetchPreviousPostsAction(topicId)),
+      fetchNext: () => dispatch(FetchNextPostsAction(topicId)),
     );
   }
 }
