ecba2c1de6d52b179fe3725bcd8c2c8e68315e3f
PoiScript <poiscript@gmail.com>
2019-12-01 20:47:37 +0800

    fix(test): fix route_test

---
+++ test/router_test.dart
@@ -0,0 +1,18 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:ngnga/main.dart';
+import 'package:ngnga/screens/welcome/welcome.dart';
+import 'package:ngnga/store/state.dart';
+
+void main() {
+  testWidgets('Route to WelcomePage if given user is null', (tester) async {
+    final store = Store(initialState: AppState.empty());
+
+    await tester.pumpWidget(MyApp(store: store));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(WelcomePage), findsOneWidget);
+  });
+}
