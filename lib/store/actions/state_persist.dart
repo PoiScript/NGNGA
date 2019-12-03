6cca8b102a20b629e4eb936d6b980def224392ee
PoiScript <poiscript@gmail.com>
2019-12-03 15:01:56 +0800

    feat(store): theme and locale settings persisting

--- lib/store/actions/state_persist.dart
+++ lib/store/actions/state_persist.dart
@@ -8,6 +8,30 @@ import 'package:path_provider/path_provider.dart';
 
 import 'package:ngnga/store/state.dart';
 
+const _stringToTheme = {
+  'black': AppTheme.black,
+  'white': AppTheme.white,
+  'yellow': AppTheme.yellow,
+  'grey': AppTheme.grey,
+};
+
+const _themeToString = {
+  AppTheme.white: 'white',
+  AppTheme.black: 'black',
+  AppTheme.grey: 'grey',
+  AppTheme.yellow: 'yellow',
+};
+
+const _stringToLocale = {
+  'en': AppLocale.en,
+  'zh': AppLocale.zh,
+};
+
+const _localeToString = {
+  AppLocale.en: 'en',
+  AppLocale.zh: 'zh',
+};
+
 class SaveState extends ReduxAction<AppState> {
   @override
   Future<AppState> reduce() async {
@@ -17,6 +41,8 @@ class SaveState extends ReduxAction<AppState> {
     String json = jsonEncode({
       'user': state.userState.toJson(),
       'baseUrl': state.settings.baseUrl,
+      'locale': _localeToString[state.settings.locale],
+      'theme': _themeToString[state.settings.theme],
       'pinned': state.pinned
           .map((id) => state.categories[id])
           .map(
@@ -64,6 +90,8 @@ class LoadState extends ReduxAction<AppState> {
             json['user'] != null ? UserState.fromJson(json['user']) : null,
         settings: state.settings.copy(
           baseUrl: json['baseUrl'],
+          theme: _stringToTheme[json['theme']],
+          locale: _stringToLocale[json['locale']],
         ),
         pinned: state.pinned..addAll(categories.map((category) => category.id)),
         categories: state.categories
