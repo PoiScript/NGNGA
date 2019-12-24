f162d962bffc8d4d0b31400366b47543c7f6e022
PoiScript <poiscript@gmail.com>
2019-12-24 15:40:54 +0800

    refactor: listen to different stream based on duration

--- client/lib/widgets/distance_to_now.dart
+++ client/lib/widgets/distance_to_now.dart
@@ -2,12 +2,22 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+final _everySeconds = StreamController<DateTime>.broadcast()
+  ..addStream(
+    Stream.periodic(Duration(seconds: 1), (_) => DateTime.now()),
+  );
+
 final _everyMinutes = StreamController<DateTime>.broadcast()
   ..addStream(
     Stream.periodic(Duration(minutes: 1), (_) => DateTime.now()),
   );
 
-class DistanceToNow extends StatelessWidget {
+final _everyHours = StreamController<DateTime>.broadcast()
+  ..addStream(
+    Stream.periodic(Duration(hours: 1), (_) => DateTime.now()),
+  );
+
+class DistanceToNow extends StatefulWidget {
   final DateTime dateTime;
 
   const DistanceToNow(
@@ -16,50 +26,113 @@ class DistanceToNow extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<DateTime>(
-      initialData: DateTime.now(),
-      stream: _everyMinutes.stream,
-      builder: (context, snapshot) => Text(
-        _distance(snapshot.data, dateTime),
-        style: Theme.of(context).textTheme.caption,
-      ),
-    );
+  _DistanceToNowState createState() => _DistanceToNowState();
+}
+
+class _DistanceToNowState extends State<DistanceToNow> {
+  StreamSubscription<DateTime> _subscription;
+  String text;
+  Interval interval;
+
+  @override
+  void initState() {
+    DateTime now = DateTime.now();
+    Duration duration = now.difference(widget.dateTime);
+    setState(() {
+      text = duration.inDays > 10 ? _date(now) : _distance(duration);
+    });
+    if (duration.inHours >= 1) {
+      _subscribeToHours();
+    } else if (duration.inMinutes >= 1) {
+      _subscribeToMinutes();
+    } else {
+      _subscribeToSeconds();
+    }
+    super.initState();
   }
 
-  String _distance(DateTime left, DateTime right) {
-    Duration difference = left.difference(right);
-    if (difference.inDays > 10) {
-      return {
-        DateTime.january: () => 'Jan \'${right.day}',
-        DateTime.february: () => 'Feb \'${right.day}',
-        DateTime.march: () => 'Mar \'${right.day}',
-        DateTime.april: () => 'Apr \'${right.day}',
-        DateTime.may: () => 'May \'${right.day}',
-        DateTime.june: () => 'Jun \'${right.day}',
-        DateTime.july: () => 'Jul \'${right.day}',
-        DateTime.august: () => 'Aug \'${right.day}',
-        DateTime.september: () => 'Sep \'${right.day}',
-        DateTime.october: () => 'Oct \'${right.day}',
-        DateTime.november: () => 'Nov \'${right.day}',
-        DateTime.december: () => 'Dec \'${right.day}',
-      }[right.month]();
-    } else if (difference.inDays > 0) {
-      if (difference.inDays < 2) {
-        return '${difference.inDays}d${difference.inHours % 24}h';
-      } else {
-        return '${difference.inDays}d';
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: Theme.of(context).textTheme.caption);
+  }
+
+  @override
+  void dispose() {
+    _unsubscribe();
+    super.dispose();
+  }
+
+  void _subscribeToHours() {
+    assert(_subscription == null);
+    _subscription = _everyHours.stream.listen((now) {
+      Duration duration = now.difference(widget.dateTime);
+      setState(() {
+        text = duration.inDays > 10 ? _date(now) : _distance(duration);
+      });
+    });
+  }
+
+  void _subscribeToMinutes() {
+    assert(_subscription == null);
+    _subscription = _everyMinutes.stream.listen((now) {
+      Duration duration = now.difference(widget.dateTime);
+      setState(() {
+        text = _distance(duration);
+      });
+      if (duration.inHours > 0) {
+        _unsubscribe();
+        _subscribeToHours();
       }
-    } else if (difference.inHours > 0) {
-      if (difference.inHours < 2) {
-        return '${difference.inHours}h${difference.inMinutes % 60}m';
-      } else {
-        return '${difference.inHours}h';
+    });
+  }
+
+  void _subscribeToSeconds() {
+    assert(_subscription == null);
+    _subscription = _everySeconds.stream.listen((now) {
+      Duration duration = now.difference(widget.dateTime);
+      setState(() {
+        text = _distance(duration);
+      });
+      if (duration.inMinutes > 0) {
+        _unsubscribe();
+        _subscribeToMinutes();
       }
-    } else if (difference.inMinutes > 0) {
-      return '${difference.inMinutes}m';
-    } else {
-      return 'now';
+    });
+  }
+
+  void _unsubscribe() {
+    if (_subscription != null) {
+      _subscription.cancel();
+      _subscription = null;
     }
+  }
+
+  String _distance(Duration duration) {
+    if (duration.inDays > 0) {
+      return '${duration.inDays}d';
+    } else if (duration.inHours > 0) {
+      return '${duration.inHours}h';
+    } else if (duration.inMinutes > 0) {
+      return '${duration.inMinutes}m';
+    } else {
+      return '${duration.inSeconds}s';
+    }
+  }
+
+  String _date(DateTime dateTime) {
+    return {
+      DateTime.january: () => 'Jan ${dateTime.day}',
+      DateTime.february: () => 'Feb ${dateTime.day}',
+      DateTime.march: () => 'Mar ${dateTime.day}',
+      DateTime.april: () => 'Apr ${dateTime.day}',
+      DateTime.may: () => 'May ${dateTime.day}',
+      DateTime.june: () => 'Jun ${dateTime.day}',
+      DateTime.july: () => 'Jul ${dateTime.day}',
+      DateTime.august: () => 'Aug ${dateTime.day}',
+      DateTime.september: () => 'Sep ${dateTime.day}',
+      DateTime.october: () => 'Oct ${dateTime.day}',
+      DateTime.november: () => 'Nov ${dateTime.day}',
+      DateTime.december: () => 'Dec ${dateTime.day}',
+    }[dateTime.month]();
   }
 }
